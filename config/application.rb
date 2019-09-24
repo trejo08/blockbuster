66281c605322edb2fbd7dc30e88f6afdf3c69471
Juan Trejo <trejocola@gmail.com>
2019-09-24 09:15:54 -0500

    comments to prevent errors

--- config/application.rb
+++ config/application.rb
@@ -14,13 +14,13 @@ require 'boot'
 
 Bundler.require :default, ENV['RACK_ENV']
 
+# Add extension and initialize database before models in order to works as well
 Sequel.extension :seed
 DB = Sequel.connect(ENV['DATABASE_URL'], logger: Logger.new(STDOUT))
 
 Dir[File.expand_path('../../api/*.rb', __FILE__), File.expand_path('../../app/*/*.rb', __FILE__)].each do |f|
   require f
 end
-puts "==================> current dir: #{__dir__}"
 Sequel::Seeder.apply(DB, "#{__dir__}/seeds/")
 
 require 'api'
